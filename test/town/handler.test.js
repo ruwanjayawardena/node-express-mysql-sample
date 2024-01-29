51006dd910887a70379ef43e0ae20713deea2540
Ruwan Jayawardena <ruwan@calliaflowers.com>
2024-01-29 01:29:00 -0600

    update project with jest and supertest test case

---
+++ test/town/handler.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const express = require('express');
+const router = require('../../src/routes/town.routes.js');
+
+const app = new express();
+router(app);
+
+describe("Town API Test with Routes", () => {
+
+    test("Get All Towns", async () => {
+        const res = await request(app).get('/town');
+        expect(res.header['content-type']).toMatch(/json/);
+        console.log('Results :: ',res._body);
+        expect(res.statusCode).toBe(200);
+    });
+
+});
